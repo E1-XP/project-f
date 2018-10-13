d819f3c49ba99d7c7d5dd8860a649b132a93cd78
E1-XP <poczta.elektroniczna@hotmail.com>
2018-10-13 19:47:12 +0200

    make before-after slider work

--- src/components/ba-slider/index.ts
+++ src/components/ba-slider/index.ts
@@ -12,7 +12,22 @@ export interface Props {
 export class CompareSlider extends Component {
   props = ["images", "currentSlide"];
 
+  resizerBtn: HTMLElement | null = null;
+  afterDiv: HTMLElement | null = null;
+  beforeDiv: HTMLElement | null = null;
+
   onMount = () => {
+    this.resizerBtn = document.getElementById("js-ba-resize");
+    this.beforeDiv = document.getElementById("js-ba-before");
+    this.afterDiv = document.getElementById("js-ba-after");
+
+    this.resizerBtn &&
+      ["mousedown", "touchstart"].forEach(
+        evt =>
+          this.resizerBtn &&
+          this.resizerBtn.addEventListener(evt, this.onMouseDown)
+      );
+
     this.addThumbnailListeners();
   };
 
@@ -22,6 +37,38 @@ export class CompareSlider extends Component {
 
   onUnmount = () => {
     this.removeThumbnailListeners();
+
+    this.resizerBtn &&
+      ["mousedown", "touchstart"].forEach(
+        evt =>
+          this.resizerBtn &&
+          this.resizerBtn.removeEventListener(evt, this.onMouseDown)
+      );
+  };
+
+  onMouseDown = (e: any) => {
+    console.log(e.pageY);
+    window.addEventListener("mousemove", this.onMouseMove);
+    window.addEventListener("mouseup", this.onMouseUp);
+  };
+
+  getWidth = (e: any) => {
+    if (!this.beforeDiv) throw new Error("DOM sync error in ba-slider");
+
+    const beforeDivDim = this.beforeDiv.getBoundingClientRect();
+    const newPos = e.pageX - beforeDivDim.left;
+
+    if (newPos > beforeDivDim.width) return 100;
+    if (newPos < 0) return 0;
+    return (newPos / beforeDivDim.width) * 100;
+  };
+
+  onMouseMove = (e: any) => {
+    if (this.afterDiv) this.afterDiv.style.width = `${this.getWidth(e)}%`;
+  };
+
+  onMouseUp = () => {
+    window.removeEventListener("mousemove", this.onMouseMove);
   };
 
   getClassNames = (idx: number) => {
@@ -32,31 +79,11 @@ export class CompareSlider extends Component {
     }
 
     const isCurrent = idx === currentSlide;
-    const backSlideIdx = currentSlide === 0 ? length - 1 : currentSlide - 1;
+    const backSlideIdx = !currentSlide ? length - 1 : currentSlide - 1;
 
     return `content__item${isCurrent ? " active" : ""}${
       idx === backSlideIdx ? " back" : ""
     }`;
-  };
-
-  getImageList = () => {
-    const { images, currentSlide } = this.model.getState();
-    const URL = `https://boiling-citadel-14104.herokuapp.com`;
-
-    if (currentSlide === undefined) throw new Error("cant find model state");
-
-    if (!images) return "";
-
-    return `<div class="ba_content" style="background:url('${URL}/${
-      images[currentSlide].dir
-    }')">
-        <div class="ba_content__after_container" style="background:url('${URL}/${
-      images[currentSlide + 1].dir
-    }')">
-            <span class="after_container__resize" id="js-ba-resize">
-            </span>
-        </div>
-    </div>`;
   };
 
   addThumbnailListeners = () => {
@@ -85,6 +112,24 @@ export class CompareSlider extends Component {
     const currentSlide = Number(e.target.closest("li").dataset.idx) - 1;
 
     this.model.setState({ currentSlide });
+  };
+
+  getImageList = () => {
+    const { images, currentSlide } = this.model.getState();
+    const URL = `https://boiling-citadel-14104.herokuapp.com`;
+
+    if (currentSlide === undefined) throw new Error("cant find model state");
+
+    if (!images) return "";
+
+    return `<div class="ba_content" id="js-ba-before"
+    style="background:url('${URL}/${images[currentSlide].dir}')">
+        <div class="ba_content__after_container" id="js-ba-after"
+        style="background:url('${URL}/${images[currentSlide + 1].dir}')">
+            <span class="after_container__resize" id="js-ba-resize">
+            </span>
+        </div>
+    </div>`;
   };
 
   getThumbnails = () => {
