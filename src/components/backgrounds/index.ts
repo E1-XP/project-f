16172fe3dbaa41f6aa99d44e9e1da4a600dd6854
E1-XP <poczta.elektroniczna@hotmail.com>
2018-10-26 20:02:45 +0200

    make sure image/background transitions behave properly

--- src/components/backgrounds/index.ts
+++ src/components/backgrounds/index.ts
@@ -6,36 +6,48 @@ export interface Props {}
 export class PageBackgrounds extends Component {
   props = ["currentSlide"];
 
+  active: HTMLElement | null = null;
+  prevActiveStyle: string = "";
+  back: HTMLElement | null = null;
+
   onMount = () => {
+    this.active = document.querySelector(".page_background__item.active div");
+    this.back = document.querySelector(".page_background__item.back div");
+
+    this.setBackground();
+  };
+
+  onUpdate = () => {
+    this.active = document.querySelector(".page_background__item.active div");
+    this.back = document.querySelector(".page_background__item.back div");
+
+    this.setBackground();
+  };
+
+  setBackground = () => {
     const { extractedColors, currentSlide } = this.model.getState();
 
-    const active = <HTMLElement>(
-      document.querySelector(".page_background__item.active div")
-    );
-
-    console.log(active, extractedColors, "COLORS",currentSlide);
-
-    if (currentSlide === undefined || !active || !extractedColors) {
+    if (
+      currentSlide === undefined ||
+      !this.active ||
+      !this.back ||
+      !extractedColors
+    ) {
       throw new Error("page backgrounds crashed.");
     }
 
     const val = extractedColors[currentSlide].DarkMuted._rgb.join(",");
     const val2 = extractedColors[currentSlide].Muted._rgb.join(",");
 
-    active.style.background = `linear-gradient(to top,#232526, rgb(${val}),rgb(${val2}))`;
-  };
-
-  onUpdate = () => {
-    this.onMount();
+    this.back.style.background = this.prevActiveStyle;
+    this.active.style.background = `linear-gradient(to top,#232526, rgb(${val}),rgb(${val2}))`;
+    this.prevActiveStyle = this.active.style.background;
   };
 
   render() {
     return html`
         <ul class="page_background">
             <li class="page_background__item active">
-                <div></div>
-            </li>
-            <li class="page_background__item">
                 <div></div>
             </li>
             <li class="page_background__item back">
